2d9f2a5e6c23225ca84851f2b2be3706913e509a
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2023-05-19 10:19:52 +0200

    components, pages
    components and pages are created and imported to app class, app.scss is also created.

---
+++ client/my-shop/src/components/Cart/Cart.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import "./Cart.scss"
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { useSelector } from 'react-redux';
+import { removeItem, resetCart } from '../../redux/cartReducer';
+import { useDispatch } from 'react-redux';
+import {loadStripe} from '@stripe/stripe-js';
+import {makeRequest} from "../../makeRequest";
+
+
+const Cart = () => {
+  const products = useSelector(state=>state.cart.products);
+  const dispatch = useDispatch();
+  
+
+  const totalPrice = ()=>{
+    let total = 0
+    products.forEach((item)=>( total += item.quantity * item.price));
+    return total.toFixed(2);
+  };
+
+  const stripePromise = loadStripe('pk_test_51MKUJIIKkWbTPK4J0WrJhnP8DB8Z4weJpgfVhQoeQWqUXL34uNelm3ebDMT3vsoXcuex6KKGBW69CfG9iNP5G9MO00GiZGhnJE');
+
+  const handlePayment = async()=>{
+    try{
+      const stripe = await stripePromise;
+
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+
+      await stripe.redirectToCheckout({
+        sessionId:res.data.stripeSession.id,
+      })
+    }catch(err){
+      console.log(err)
+    }
+  }
+  
+  return (
+    <div className='cart'>Cart
+      <h1>Products in your cart</h1>
+      {products?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={process.env.REACT_APP_UPLOAD_URL + item.img} alt="" />
+          <div className="details">
+            <h1>{item.title}</h1>
+            <p>{item.desc?.substring(0,100)}</p>
+            <div className="price">{item.quantity} x {item.price} SEK</div>
+          </div>
+          <DeleteOutlineIcon  className="delete" onClick={()=>dispatch(removeItem(item.id))}/>
+        </div>
+      ))}
+     <div className="total">
+       <span>TOTAL</span>
+       <span>{totalPrice()} SEK</span>
+     </div> 
+     <button onClick={handlePayment}>Go to Checkout</button>
+     <span className="reset" onClick={() => dispatch(resetCart())}>
+      Reset Cart
+     </span>
+    </div>
+  )
+}
+export default Cart
---
+++ client/my-shop/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import {Link} from "react-router-dom";
+import "./Navbar.scss"
+import Cart from '../Cart/Cart';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+  const [open,setOpen] = useState(false)
+  const products = useSelector(state=>state.cart.products);
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+         <div className="left">
+            <div className="item">
+               <img src="/img/se.png" alt="" />
+                 <KeyboardArrowDownIcon />
+            </div>
+
+            <div className='item'>
+                <span>SEK</span>
+                <KeyboardArrowDownIcon/>
+            </div>
+          
+             <div className="item">
+                 <Link className="link" to ="/products/1">Women</Link>
+             </div>
+
+             <div className="item">
+                 <Link className="link" to ="/products/2">Men</Link>
+             </div>
+
+             <div className="item">
+                 <Link className="link" to ="/products/3">Children</Link>
+              </div>
+          </div>
+
+          <div className='center'>
+            <Link className="link" to ="/">NJShop </Link>
+          </div>
+
+         <div className='right'>
+
+              <div className='item'>
+                 <Link className="link" to ="/">Homepage </Link>
+              </div>
+
+              <div className='item'>
+                  <Link className="link" to ="/">About </Link>
+              </div>
+
+              <div className='item'>
+                   <Link className="link" to ="/"> Contact </Link>
+              </div>
+
+              <div className='item'>
+                  <Link className="link" to ="/">Stores </Link>
+              </div>
+
+              <div className="icons">
+                <SearchIcon />
+                <PersonIcon />
+                <FavoriteBorderIcon />
+              <div className="cartIcon" onClick={()=>setOpen(!open)}>
+                <ShoppingCartIcon/>
+                <span>{products.length}</span>
+              </div>
+            </div>
+         </div>
+
+      </div>
+      {open && <Cart/>}
+    </div>
+  );
+};
+export default Navbar;
