43e74ee53f30c5e48dd828752e86604affe9efd0
Nishat7155 <57759578+Nishat7155@users.noreply.github.com>
2023-05-22 23:32:18 +0200

    updated
    updated list.jsx file

--- client/my-shop/src/App.js
+++ client/my-shop/src/App.js
@@ -1,56 +1,49 @@
-import {
-  createBrowserRouter,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
-
-import Home from "./pages/Home/Home";
-import Products from "./pages/Product/Product";
-import Product from "./pages/Products/Products";
+import { Children } from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
 import "./App.scss"
 
-const Layout = ()=> {
-  return(
-   <div className="app">
-     <Navbar/>
-     <Outlet/>
-     <Footer/>
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
-   </div>
-  )
-}
-
-
-const router = createBrowserRouter ([
-  
+const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Layout/>,
-    children:[
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        path:"/",
-        element:<Home/>,
+        path: "/",
+        element: <Home />,
       },
       {
-        path:"/products/:id",
-        element:<Products/>,
+        path: "/products/:id",
+        element: <Products />,
       },
       {
-        path:"/product/:id",
-        element:<Product/>,
+        path: "/product/:id",
+        element: <Product />,
       },
     ],
   },
-  
 ]);
+
 function App() {
   return (
-    <div >
+    <div>
       <RouterProvider router={router} />
     </div>
   );
-}
+};
 
 export default App;
